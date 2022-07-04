e5254a82deb1bd53d551ce6e567ed53ea7e7b6e8
ANDERSON MENDES DOS SANTOS <anderson.mendes@tjpe.gov.br>
2022-07-04 13:07:17 -0300

    tjpe

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-const googleStrategy = require("passport-google-oidc").Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
@@ -45,8 +45,10 @@ passport.serializeUser(User.serializeUser());
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/secrets'
-},function (accessToken, refreshToken profile, cb) {
+    callbackURL: 'http://localhost:3000/auth/google/secrets',
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
         User.findOrCreate({ googleId: profile.id }, function (err, user) {
 
         });
@@ -77,6 +79,15 @@ app.get("/logout", function (req, res) {
     req.logout;
     res.redirect("/login");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: ["/login"] }),
+    function (req, res) {
+        res.redirect("/secrets");
+    });
 
 app.post("/register", function (req, res) {
     console.log();
